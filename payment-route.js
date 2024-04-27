a42a472fccb5f9180700563b49f73272793cb088
ShaikArshad2004 <108758411+ShaikArshad2004@users.noreply.github.com>
2024-04-27 07:30:11 +0530

    Add files via upload

---
+++ payment-route.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { addPayment, getAllPayment, getPaymentOnNumber } from '../controllers/payment-controllers.js';
+
+const paymentRouter= express.Router();
+
+paymentRouter.get('/', getAllPayment);
+paymentRouter.post('/addPayment', addPayment);
+paymentRouter.post('/getPayments', getPaymentOnNumber);
+
+
+export default paymentRouter;
